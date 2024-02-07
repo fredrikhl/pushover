e464214fffcdda2e579fda23af71048f235e4a41
Fredrik Larsen <fredrik.h.larsen@gmail.com>
2024-02-07 14:05:30 +0100

    Re-do packaging a bit

--- setup.py
+++ setup.py
@@ -1,20 +1,5 @@
-#!/usr/bin/env python2
-# encoding: utf-8
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+import setuptools
 
-version = "1.1"
-
-setup(
-    name="pushover",
-    author="Fredrik Larsen",
-    author_email="fredrik.h.larsen@gmail.com",
-    url="https://github.com/fredrikhl/pushover",
-    description="Simple pushover cli program and library.",
-    entry_points={
-        "setuptools.installation": ["eggsecutable = pushover:main"],
-        "console_scripts": ["pushover = pushover.__main__:main"],
-    },
-    version=version,
-    license="GPL",
-    packages=find_packages(),
-)
+if __name__ == '__main__':
+    setuptools.setup()
